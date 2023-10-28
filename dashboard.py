f8da021eacb40949ffa49345bbf59f229fcf869e
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2023-10-28 23:22:18 +0530

    Add exercise dataset in data folder and modify dashboard.py to make it interactive

--- dashboard.py
+++ dashboard.py
@@ -90,84 +90,180 @@
 
 
 
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import plotly.express as px
+# import matplotlib.pyplot as plt
+
+# np.random.seed(42)
+# dates_30 = pd.date_range(start="2023-10-01", end="2023-10-31")
+# dates_90 = pd.date_range(start="2023-08-01", end="2023-10-31")
+# dates_180 = pd.date_range(start="2023-05-01", end="2023-10-31")
+# dates_365 = pd.date_range(start="2023-01-01", end="2023-12-31")
+# weight_30 = np.random.normal(loc=70, scale=5, size=len(dates_30))
+# weight_90 = np.random.normal(loc=70, scale=5, size=len(dates_90))
+# weight_180 = np.random.normal(loc=70, scale=5, size=len(dates_180))
+# weight_365 = np.random.normal(loc=70, scale=5, size=len(dates_365))
+
+# df_30 = pd.DataFrame({"Date": dates_30, "Weight": weight_30})
+# df_90 = pd.DataFrame({"Date": dates_90, "Weight": weight_90})
+# df_180 = pd.DataFrame({"Date": dates_180, "Weight": weight_180})
+# df_365 = pd.DataFrame({"Date": dates_365, "Weight": weight_365})
+
+# def get_reps(weight):
+#     np.random.seed(int(42))
+#     correct = np.random.randint(0, 11)
+#     wrong = 10 - correct
+#     return (correct, wrong)
+
+# st.set_page_config(
+#     page_title="Weight Dashboard",
+#     page_icon="🏋️‍♂️",
+#     layout="wide",
+#     initial_sidebar_state="expanded"
+# )
+
+# st.title("Weight Dashboard")
+# st.subheader("Track your weight progress with Streamlit")
+
+# col1, col2 = st.columns(2)
+# col1.subheader("Select the period of interest")
+# period = col1.selectbox("", ["Last 30 days", "Last 3 months", "Last 6 months", "Last year"])
+
+# # period = st.selectbox("Select the period of interest", ["Last 30 days", "Last 3 months", "Last 6 months", "Last year"])
+
+# period_dict = {
+#     "Last 30 days": df_30,
+#     "Last 3 months": df_90,
+#     "Last 6 months": df_180,
+#     "Last year": df_365
+# }
+
+# df = period_dict[period]
+
+# weight_line = px.line(df, x="Date", y="Weight", color_discrete_sequence=["blue"])
+# weight_line.update_layout(title=f"Weight Progress - {period}", title_x=0.5)
+
+
+# col2.subheader("Select a date of interest")
+# date = col2.date_input("", min_value=df["Date"].min(), max_value=df["Date"].max())
+# # date = st.date_input("Select a date of interest", min_value=df["Date"].min(), max_value=df["Date"].max())
+
+# weight = df[df["Date"] == date]["Weight"].values
+
+# if weight.size != 0:
+#     weight = weight[0]
+# else:
+#     weight = None 
+
+# correct, wrong = get_reps(weight)
+
+# fig, ax = plt.subplots() 
+# ax.pie([correct, wrong], labels=["Correct", "Wrong"], autopct="%1.1f%%", colors=["green", "red"])
+# ax.set_title(f"Reps Performance - {date}")
+
+# # create two columns with equal width
+
+# # display the line plot in the first column
+# col1.plotly_chart(weight_line)
+
+# # display the pie plot in the second column
+# col2.pyplot(fig)
+
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-import matplotlib.pyplot as plt
+from vega_datasets import data
+import altair as alt
+import plotly.graph_objects as go
 
-np.random.seed(42)
+
+df = pd.read_csv("data\exercise_dataset.csv")
+
+df = df.drop(columns=['Age', 'Gender', 'Duration', 'Weather Conditions'])
+
+st.set_page_config(
+    page_title="Your Personal Fitness Dashboard",
+    page_icon="🏋️‍♂️",
+)
+
 dates_30 = pd.date_range(start="2023-10-01", end="2023-10-31")
 dates_90 = pd.date_range(start="2023-08-01", end="2023-10-31")
 dates_180 = pd.date_range(start="2023-05-01", end="2023-10-31")
 dates_365 = pd.date_range(start="2023-01-01", end="2023-12-31")
-weight_30 = np.random.normal(loc=70, scale=5, size=len(dates_30))
-weight_90 = np.random.normal(loc=70, scale=5, size=len(dates_90))
-weight_180 = np.random.normal(loc=70, scale=5, size=len(dates_180))
-weight_365 = np.random.normal(loc=70, scale=5, size=len(dates_365))
 
-df_30 = pd.DataFrame({"Date": dates_30, "Weight": weight_30})
-df_90 = pd.DataFrame({"Date": dates_90, "Weight": weight_90})
-df_180 = pd.DataFrame({"Date": dates_180, "Weight": weight_180})
-df_365 = pd.DataFrame({"Date": dates_365, "Weight": weight_365})
+# Display the list of columns in the dataset
+st.write('### Columns in the Dataset')
+st.table(df.columns[1:])
 
-def get_reps(weight):
-    np.random.seed(int(42))
-    correct = np.random.randint(0, 11)
-    wrong = 10 - correct
-    return (correct, wrong)
+# User selects two columns for the chart
+selected_columns = st.multiselect('Select Two Columns', df.columns)
 
-st.set_page_config(
-    page_title="Weight Dashboard",
-    page_icon="🏋️‍♂️",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+# User selects the type of chart
+chart_type = st.selectbox('Select Chart Type', ['Area Chart', 'Heatmap'])
 
-st.title("Weight Dashboard")
-st.subheader("Track your weight progress with Streamlit")
+if selected_columns and chart_type:
+    try:
+        if chart_type == 'Area Chart':
+            st.write('### Area Chart')
+            chart = alt.Chart(df).mark_area().encode(
+                x=selected_columns[0],
+                y=selected_columns[1]
+            )
+            st.altair_chart(chart, use_container_width=True)
 
-col1, col2 = st.columns(2)
-col1.subheader("Select the period of interest")
-period = col1.selectbox("", ["Last 30 days", "Last 3 months", "Last 6 months", "Last year"])
+        elif chart_type == 'Heatmap':
+            st.write('### Heatmap')
+            heatmap = alt.Chart(df).mark_rect().encode(
+                x=alt.X(selected_columns[0], title=selected_columns[0]),
+                y=alt.Y(selected_columns[1], title=selected_columns[1]),
+                color=alt.Color(selected_columns[1], scale=alt.Scale(scheme='redyellowgreen'))
+            )
+            st.altair_chart(heatmap, use_container_width=True)
 
-# period = st.selectbox("Select the period of interest", ["Last 30 days", "Last 3 months", "Last 6 months", "Last year"])
+    except Exception as e:
+        st.error("An error occurred while creating the chart. Please check your selections.")
 
-period_dict = {
-    "Last 30 days": df_30,
-    "Last 3 months": df_90,
-    "Last 6 months": df_180,
-    "Last year": df_365
-}
+# User selects two columns for the chart
+selected_columns = st.multiselect('Select Four Columns', df.columns)
 
-df = period_dict[period]
+# User selects the type of chart
+chart_type = st.selectbox('Select Chart Type', ['Bubble Plot', 'Radar Plot'])
 
-weight_line = px.line(df, x="Date", y="Weight", color_discrete_sequence=["blue"])
-weight_line.update_layout(title=f"Weight Progress - {period}", title_x=0.5)
+if selected_columns and chart_type:
+    try:
+        if chart_type == 'Radar Plot':
+            fig = go.Figure()
+
+            for column in selected_columns:
+                fig.add_trace(go.Scatterpolar(
+                    r=df[column],
+                    theta=selected_columns,
+                    fill='toself',
+                    name=column
+                ))
+
+            fig.update_layout(
+                polar=dict(
+                    radialaxis=dict(
+                        visible=True,
+                    ),
+                ),
+            )
+
+            st.plotly_chart(fig, use_container_width=True)
 
 
-col2.subheader("Select a date of interest")
-date = col2.date_input("", min_value=df["Date"].min(), max_value=df["Date"].max())
-# date = st.date_input("Select a date of interest", min_value=df["Date"].min(), max_value=df["Date"].max())
+        elif chart_type == 'Bubble Plot':
+            st.write('### Bubble Plot')
+            bubble_chart = alt.Chart(df).mark_circle().encode(
+                x=alt.X(selected_columns[0]),
+                y=alt.Y(selected_columns[1]),
+                size=alt.Size(selected_columns[2], scale=alt.Scale(range=[100, 300])),
+                color=alt.Color(selected_columns[3], scale=alt.Scale(scheme='category20'))
+            ).interactive()
+            st.altair_chart(bubble_chart, use_container_width=True)
 
-weight = df[df["Date"] == date]["Weight"].values
-
-if weight.size != 0:
-    weight = weight[0]
-else:
-    weight = None 
-
-correct, wrong = get_reps(weight)
-
-fig, ax = plt.subplots() 
-ax.pie([correct, wrong], labels=["Correct", "Wrong"], autopct="%1.1f%%", colors=["green", "red"])
-ax.set_title(f"Reps Performance - {date}")
-
-# create two columns with equal width
-
-# display the line plot in the first column
-col1.plotly_chart(weight_line)
-
-# display the pie plot in the second column
-col2.pyplot(fig)
-
+    except Exception as e:
+        st.error("An error occurred while creating the chart. Please check your selections.")
+        
